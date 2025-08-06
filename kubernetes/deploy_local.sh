9c70183d48cca2e1649fc8f3c7727010ff1865e6
okadak <okadak@sci.hokudai.ac.jp>
2025-08-06 13:16:25 +0900

    PhaseNetのサブプロジェクトコミットを更新し、新しいPodメタデータとPhaseNet APIのYAMLファイルを追加。SpecStoryのディレクトリを作成し、Kubernetesのデプロイメントスクリプトを修正。ノートブックの実行カウントをリセットし、依存関係を整理。

--- kubernetes/deploy_local.sh
+++ kubernetes/deploy_local.sh
@@ -13,14 +13,14 @@ cd ../waveform; docker build --tag quakeflow-waveform:1.0 .; cd ../kubernetes;
 cd ../streamlit; docker build --tag quakeflow-streamlit:1.0 .; cd ../kubernetes;
 
 # Deploy Kafka with Helm, create client and add topics
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install quakeflow-kafka bitnami/kafka
-kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --image docker.io/bitnami/kafka:2.7.0-debian-10-r68 --restart=Never \
-    --command -- bash -c "kafka-topics.sh --create --topic phasenet_picks --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic gmma_events --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic waveform_raw --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic phasenet_waveform --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092"
-kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --image docker.io/bitnami/kafka:2.7.0-debian-10-r68 --restart=Never \
-    --command -- bash -c "kafka-configs.sh --alter --entity-type topics --entity-name phasenet_picks --add-config 'retention.ms=-1' --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-configs.sh --alter --entity-type topics --entity-name gmma_events --add-config 'retention.ms=-1' --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092"
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm install quakeflow-kafka bitnami/kafka
+#kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --image docker.io/bitnami/kafka:2.7.0-debian-10-r68 --restart=Never \
+#    --command -- bash -c "kafka-topics.sh --create --topic phasenet_picks --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic gmma_events --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic waveform_raw --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic phasenet_waveform --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092"
+#kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --image docker.io/bitnami/kafka:2.7.0-debian-10-r68 --restart=Never \
+#    --command -- bash -c "kafka-configs.sh --alter --entity-type topics --entity-name phasenet_picks --add-config 'retention.ms=-1' --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-configs.sh --alter --entity-type topics --entity-name gmma_events --add-config 'retention.ms=-1' --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092"
 # For external access (not safe):
-helm upgrade quakeflow-kafka bitnami/kafka --set externalAccess.enabled=true,externalAccess.autoDiscovery.enabled=true,rbac.create=true
+# helm upgrade quakeflow-kafka bitnami/kafka --set externalAccess.enabled=true,externalAccess.autoDiscovery.enabled=true,rbac.create=true
 # # Check topic configs:
 # kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --image docker.io/bitnami/kafka:2.7.0-debian-10-r68 --restart=Never \
 #     --command -- bash -c "kafka-topics.sh --describe --topics-with-overrides --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092"
@@ -46,5 +46,4 @@ kubectl autoscale deployment gmma-api --cpu-percent=80 --min=1 --max=10
 while true; do kubectl port-forward svc/phasenet-api 8001:8001 --address='0.0.0.0'; done & \
 while true; do kubectl port-forward svc/gamma-api 8002:8002 --address='0.0.0.0'; done  & \
 while true; do kubectl port-forward svc/deepdenoiser-api 8003:8003 --address='0.0.0.0'; done  & \
-while true; do kubeflow port-forward svc/ml-pipeline-ui 8080:80 --address='0.0.0.0'; done  &
-# while true; do kubeflow port-forward svc/ml-pipeline-ui 8080:80 --address='0.0.0.0'; done  &
+while true; do kubectl port-forward -n kubeflow svc/ml-pipeline-ui 8080:80 --address='0.0.0.0'; done  &
